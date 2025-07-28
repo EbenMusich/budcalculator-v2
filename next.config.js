ef6f1afbdaf89c362f8c9087f13db2623eee82f6
EbenMusich <eben@budcalculator.com>
2025-07-28 14:50:57 -0400

    Add wildcard .html redirect to clean URLs

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  async redirects() {
+    return [
+      // Legacy .html redirects - redirect to clean URLs
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/:path*.html',
+        destination: '/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
